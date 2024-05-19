59a62099e9041c57c98975963a925775ee34d41f
Rhtox <krystofburda@gmail.com>
2024-05-20 00:05:35 +0200

    Basic navbar and SQLite

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const db = require('../db.js');
+
+const User = {
+    create: (firstName, lastName, callback) => {
+        db.run(`INSERT INTO users(firstName, lastName) VALUES(?, ?)`, [firstName, lastName], function(err) {
+            if (err) {
+                return callback(err);
+            }
+            callback(null, { id: this.lastID, firstName, lastName });
+        });
+    },
+    // Add more methods as needed
+};
+
+module.exports = User;
